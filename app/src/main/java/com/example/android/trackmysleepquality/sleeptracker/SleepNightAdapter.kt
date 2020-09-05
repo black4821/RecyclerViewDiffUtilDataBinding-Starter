67935762685a431faba8428b502daf634a70d3cd
black4821 <56718434+black4821@users.noreply.github.com>
2020-09-05 19:47:20 +0700

    Step 2-7-7.2

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -21,6 +21,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -83,6 +84,22 @@ class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(
                         ListItemSleepNightBinding.inflate(layoutInflater, parent, false)
 
                 return ViewHolder(binding)
+            }
+            @BindingAdapter("sleepQualityString")
+            fun TextView.setSleepQualityString(item: SleepNight) {
+                text = convertNumericQualityToString(item.sleepQuality, context.resources)
+            }
+            @BindingAdapter("sleepImage")
+            fun ImageView.setSleepImage(item: SleepNight) {
+                setImageResource(when (item.sleepQuality) {
+                    0 -> R.drawable.ic_sleep_0
+                    1 -> R.drawable.ic_sleep_1
+                    2 -> R.drawable.ic_sleep_2
+                    3 -> R.drawable.ic_sleep_3
+                    4 -> R.drawable.ic_sleep_4
+                    5 -> R.drawable.ic_sleep_5
+                    else -> R.drawable.ic_sleep_active
+                })
             }
         }
     }
